f17e1981434fb9d82753f499465834fa9aa19cc9
aron <latis.aron@gmail.com>
2025-07-13 14:19:38 -0400

    sad

--- app/main.rb
+++ app/main.rb
@@ -1,8 +1,11 @@
 # Uncomment this block to pass the first stage
-$stdout.write("$ ")
+
 
 # Wait for user input
 loop do
+  $stdout.write("$ ")
   tokens = gets.chomp.split(" ")
-  puts "#{tokens[0]}: command not found"
+  $stdout.write("#{tokens[0]}: command not found")
+  
+  $stdout.write("\n")
 end
