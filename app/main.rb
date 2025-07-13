5e35e09e6f0c199c11218a154837f5523bc654fe
aron <latis.aron@gmail.com>
2025-07-13 14:22:55 -0400

    ads

--- app/main.rb
+++ app/main.rb
@@ -1,11 +1,19 @@
 # Uncomment this block to pass the first stage
 
+ALLOWED_COMMANDS = Set.new(['exit'])
 
 # Wait for user input
 loop do
   $stdout.write("$ ")
   tokens = gets.chomp.split(" ")
-  $stdout.write("#{tokens[0]}: command not found")
-  
+  tokens.each do |token|
+    if ALLOWED_COMMANDS.include?(token)
+      if token == 'exit'
+        exit 0
+      end
+    else
+      $stdout.write("#{tokens[0]}: command not found")
+    end
+  end
   $stdout.write("\n")
 end
