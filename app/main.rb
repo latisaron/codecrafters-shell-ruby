4646b4d37d8ba87a36b4cd9e636886aca214bf11
aron <latis.aron@gmail.com>
2025-07-13 14:36:37 -0400

    sad

--- app/main.rb
+++ app/main.rb
@@ -1,5 +1,5 @@
 # Uncomment this block to pass the first stage
-ALLOWED_COMMANDS = Set.new(['exit', 'echo'])
+BUILTINS = Set.new(['exit', 'echo', 'type'])
 
 def consume(iterator)
   [].tap do |ary|
@@ -13,17 +13,38 @@ def consume(iterator)
   end
 end
 
+def exit_builtin
+  exit 0
+end
+
+def echo_builtin
+  $stdout.write(consume(@token_iterator).join(' '))
+end
+
+def type_builtin
+  arg = @token_iterator.next rescue ''
+  if BUILTINS.include?(arg)
+    $stdout.write("#{arg} is a shell builtin")
+  elsif arg.empty?
+    $stdout.write('specify an argument for type builtin')
+  else
+    $stdout.write("#{arg}: not found")
+  end
+end
+
 # Wait for user input
 loop do
   $stdout.write("$ ")
-  token_iterator = gets.chomp.enum_for(:split, ' ', -1)
+  @token_iterator = gets.chomp.enum_for(:split, ' ', -1)
   loop do
-    current_token = token_iterator.next()
+    current_token = @token_iterator.next()
     begin
       if current_token == 'exit'
-        exit 0
+        exit_builtin
       elsif current_token == 'echo'
-        $stdout.write(consume(token_iterator).join(' '))
+        echo_builtin
+      elsif current_token == 'type'
+        type_builtin
       else
         $stdout.write("#{current_token}: command not found")
         break
