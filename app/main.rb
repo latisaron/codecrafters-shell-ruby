4a3c322665ddfea06e7275697be57666137c7cd5
aron <latis.aron@gmail.com>
2025-07-13 14:31:07 -0400

    echo functional

--- app/main.rb
+++ app/main.rb
@@ -1,18 +1,35 @@
 # Uncomment this block to pass the first stage
+ALLOWED_COMMANDS = Set.new(['exit', 'echo'])
 
-ALLOWED_COMMANDS = Set.new(['exit'])
+def consume(iterator)
+  [].tap do |ary|
+    loop do
+      begin
+        ary << iterator.next
+      rescue StopIteration
+        break
+      end
+    end
+  end
+end
 
 # Wait for user input
 loop do
   $stdout.write("$ ")
-  tokens = gets.chomp.split(" ")
-  tokens.each do |token|
-    if ALLOWED_COMMANDS.include?(token)
-      if token == 'exit'
+  token_iterator = gets.chomp.enum_for(:split, ' ', -1)
+  loop do
+    current_token = token_iterator.next()
+    begin
+      if current_token == 'exit'
         exit 0
+      elsif current_token == 'echo'
+        $stdout.write(consume(token_iterator).join(' '))
+      else
+        $stdout.write("#{current_token}: command not found")
+        break
       end
-    else
-      $stdout.write("#{tokens[0]}: command not found")
+    rescue StopIteration
+      break
     end
   end
   $stdout.write("\n")
